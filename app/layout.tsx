e29807126181fdda1c491e6701141643ead85a79
MonicaNSTorres <monicanstorress@gmail.com>
2025-07-23 12:17:42 -0300

    Corrigindo erro em tela

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ClientLayout>{children}</ClientLayout>
       </body>
